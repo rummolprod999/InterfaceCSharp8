fd045628788626507a3cbed2a6564e017ab75cd2
rummolprod999 <rummolprod999@gmail.com>
2020-02-17 12:20:57 +0300

    add example

--- Program.cs
+++ Program.cs
@@ -2,11 +2,37 @@
 
 namespace InterfaceCSharp8
 {
+    interface IMovable
+    {
+        protected internal void Move();
+        protected internal string Name { get; set; }
+        delegate void MoveHandler();
+        protected internal event MoveHandler MoveEvent;
+    }
+    class Person : IMovable
+    {
+        string IMovable.Name { get; set; }
+        
+        IMovable.MoveHandler _moveEvent;
+        event IMovable.MoveHandler IMovable.MoveEvent
+        {
+            add => _moveEvent += value;
+            remove => _moveEvent -= value;
+        }
+        
+        void IMovable.Move()
+        {
+            Console.WriteLine("Person is walking"); 
+            _moveEvent();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IMovable mov = new Person();
+            mov.MoveEvent += () => Console.WriteLine("IMovable is moving");
+            mov.Move();
         }
     }
 }
